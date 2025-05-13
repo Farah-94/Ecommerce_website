f440578f2e261644f71c2f48329bd49ad0ec8b5c
Farah-94 <farahshigri94@gmail.com>
2025-05-13 03:03:47 +0100

    updated

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -76,14 +76,18 @@ WSGI_APPLICATION = 'ecommerce_store.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce_db',
-        'USER': 'postgres',
-        'PASSWORD': 'farahakbar1',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+
+'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ecommerce_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'farahakbar1',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
