c8a790c85798a85469bcf812f8d662936cb1a1a0
Farah-94 <farahshigri94@gmail.com>
2025-05-13 03:27:29 +0100

    setting.py updated

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -129,10 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / 'store/static']  # Tell Django where to find static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Default primary key field type
