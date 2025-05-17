b8815f9f15019c9cf0f37569d440e237c4a0cc11
Farah-94 <farahshigri94@gmail.com>
2025-05-17 05:01:27 +0100

    css

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -143,7 +143,11 @@ LOGIN_URL = '/signin/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'store/static')
+]
+
+
 
 
 # For production
