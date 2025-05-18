08c98de4ead1c44f3b0e79184ffe942850538424
Farah-94 <farahshigri94@gmail.com>
2025-05-18 16:43:07 +0100

    setting.py

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 
-
+SECURE_SSL_REDIRECT = False
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
